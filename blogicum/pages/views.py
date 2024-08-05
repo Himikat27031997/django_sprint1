2684a8eacc4393ede96b717876341deb3fe96cb9
Никита Казак <muskullmorf@gmail.com>
2024-08-05 22:33:08 +0300

    Конечный рабочий вариант2

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
 
 def rules(request):
     template = 'pages/rules.html'
     return render(request, template)
+
 
 def about(request):
     template = 'pages/about.html'
