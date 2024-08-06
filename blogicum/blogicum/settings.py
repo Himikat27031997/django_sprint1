38d7f1cf57d21dd03450759c22a59d92ed70aef6
Никита Казак <muskullmorf@gmail.com>
2024-08-06 21:29:33 +0300

    1

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vey1h=1p3vp6vgd!l6t&=mxu_e52-iu@t=f3tn#jzskka_7@-='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
